827b67f6f088ac7495ddf33f878a7eb91ef1f0ba
unknown <ya.ramdane01@gmail.com>
2024-08-28 17:32:20 +0200

    add ShopContext

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import ShopCategory from './Pages/ShopCategory';
 import Cart from './Pages/Cart';
 import Product from './Pages/Product';
 import LoginSignUp from './Pages/LoginSignUp';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/Frontend_Assets/banner_mens.png';
+import women_banner from './Components/Assets/Frontend_Assets/banner_women.png';
+import kids_banner from './Components/Assets/Frontend_Assets/banner_kids.png'
 
 function App() {
   return (
@@ -14,15 +18,16 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Shop/>}/>
-        <Route path="/homme" element={<ShopCategory category="homme"/>}/>
-        <Route path="/femme" element={<ShopCategory category="femme"/>}/>
-        <Route path="/enfant" element={<ShopCategory category="enfant"/>}/>
+        <Route path="/homme" element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path="/femme" element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path="/enfant" element={<ShopCategory banner={kids_banner} category="kid"/>}/>
         <Route path="/product" element={<Product/>}>
         <Route path=":productId" element={<Product/>}/>
         </Route>
         <Route path="/cart" element={<Cart/>}/>
         <Route path="/login" element={<LoginSignUp/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>  
     </div>
   );
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import './CSS/ShopCategory.css';
+import {ShopContext} from '../Context/ShopContest';
+import dropdown_icon from '../Components/Assets/Frontend_Assets/dropdown_icon.png';
+import Item from '../Components/Item/Item';
 
-const ShopCategory = () => {
+
+const ShopCategory = (props) => {
+
+    const {all_product} = useContext(ShopContext);
     return (
-        <div>
-
+        <div className='shop-category'>
+            <img src={props.banner} alt=''/>
+            <div className="shopCategory-indexSort">
+                <p>
+                    <span>De 1-12</span> sur 36
+                </p>
+                <div className="shopCategory-sort">
+                    Tri par <img className='shopCategory-banner' src={dropdown_icon} alt=''/>
+                </div>
+            </div>
+            <div className="shopCategory-products">
+                {all_product.map((item,i)=>{
+                    if (props.category===item.category){
+                        return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                    }
+                    else {
+                        return null;
+                    }
+                })}
+            </div>
         </div>
     )
 }
