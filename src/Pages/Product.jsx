229d3bca2ad2edf62d634e4ee543015048f5a180
unknown <ya.ramdane01@gmail.com>
2024-09-01 23:09:21 +0200

    add new product,jsx

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ShopContext} from '../Context/ShopContest'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
 
 const Product = () => {
+
+    const {all_product} = useContext(ShopContext);
+    const {productId} = useParams();
+    const product = all_product.find((e) => e.id === Number(productId))
+
     return (
         <div>
+            <Breadcrum product={product}/>
 
         </div>
     )
